9aefd3d7629f81539d9bbee33fd2572635d583e7
Chris Andrews <candrews@venda.com>
2015-09-04 16:42:54 +0100

    Refactor SRP implementation out into its own class.

---
+++ src/main/java/com/cryptopals/SRP.java
@@ -0,0 +1,84 @@
+package com.cryptopals;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class SRP {
+    private final Random r = new Random();
+    private final BigInteger N;
+    private final BigInteger g;
+    private final BigInteger k;
+    private CryptoBuffer I;
+    private CryptoBuffer P;
+
+    private BigInteger salt, v;
+
+    private final BigInteger privateKey;
+    private BigInteger publicKey;
+
+    public SRP() {
+        DH.Params params = new DH.Params();
+        N = params.p;
+        g = params.g;
+        k = new BigInteger("3");
+        privateKey = new BigInteger(512, r);
+    }
+
+    public void setCredentials(String user, String password) {
+        this.I = new CryptoBuffer(user);
+        this.P = new CryptoBuffer(password);
+    }
+
+    // client methods
+
+    public BigInteger clientComputePublicKey() {
+        publicKey = g.modPow(privateKey, N);
+        return publicKey;
+    }
+
+    public CryptoBuffer clientComputeSessionKey(BigInteger serverPublicKey) {
+        CryptoBuffer uH = new CryptoBuffer(publicKey.toByteArray()).append(new CryptoBuffer(serverPublicKey.toByteArray())).sha256();
+        BigInteger u = new BigInteger(uH.toHex(), 16);
+        CryptoBuffer xH = new CryptoBuffer(salt.toByteArray()).append(P).sha256();
+        BigInteger x = new BigInteger(xH.toHex(), 16);
+        BigInteger S = serverPublicKey.subtract(k.multiply(g.modPow(x, N))).modPow((privateKey.add(u.multiply(x))), N);
+        CryptoBuffer K = new CryptoBuffer(S.toByteArray()).sha256();
+        return MACs.hmacSha256(K, new CryptoBuffer(salt.toByteArray()));
+    }
+
+    // server methods
+
+    public void serverComputeVerifier() {
+        salt = new BigInteger(512, r);
+        CryptoBuffer xH = new CryptoBuffer(salt.toByteArray()).append(P).sha256();
+        BigInteger x = new BigInteger(xH.toHex(), 16);
+        v = g.modPow(x, N);
+    }
+
+    public BigInteger serverComputePublicKey() {
+        publicKey = (k.multiply(v)).add(g.modPow(privateKey, N));
+        return publicKey;
+    }
+
+    public CryptoBuffer serverComputeSessionKey(BigInteger clientPublicKey) {
+        CryptoBuffer uH = new CryptoBuffer(clientPublicKey.toByteArray()).append(new CryptoBuffer(publicKey.toByteArray())).sha256();
+        BigInteger u = new BigInteger(uH.toHex(), 16);
+        BigInteger S = clientPublicKey.multiply(v.modPow(u, N)).modPow(privateKey, N);
+        CryptoBuffer K = new CryptoBuffer(S.toByteArray()).sha256();
+        return MACs.hmacSha256(K, new CryptoBuffer(salt.toByteArray()));
+    }
+
+    // getters/setters
+
+    public CryptoBuffer getI() {
+        return I;
+    }
+
+    public void setSalt(BigInteger salt) {
+        this.salt = salt;
+    }
+
+    public BigInteger getSalt() {
+        return salt;
+    }
+}
--- src/test/java/com/cryptopals/Set5Challenge36.java
+++ src/test/java/com/cryptopals/Set5Challenge36.java
@@ -3,14 +3,13 @@ package com.cryptopals;
 import org.junit.Test;
 
 import java.math.BigInteger;
-import java.util.Random;
 
 public class Set5Challenge36 {
 
     @Test
     public void testSRP() {
-        Client C = new Client("joshua");
-        Server S = new Server();
+        Client C = new Client("falken", "joshua");
+        Server S = new Server("falken", "joshua");
 
         C.sendPublicKey(S);
         S.sendPublicKeyReply(C);
@@ -23,8 +22,8 @@ public class Set5Challenge36 {
 
     @Test
     public void testSRPBadPassword() {
-        Client C = new Client("xyzzy");
-        Server S = new Server();
+        Client C = new Client("falken", "xyzzy");
+        Server S = new Server("falken", "joshua");
 
         C.sendPublicKey(S);
         S.sendPublicKeyReply(C);
@@ -36,20 +35,12 @@ public class Set5Challenge36 {
     }
 
     public static abstract class Endpoint {
-        protected final Random r = new Random();
-        protected final BigInteger N;
-        protected final BigInteger g;
-        protected final BigInteger k;
-        protected CryptoBuffer I;
-        protected CryptoBuffer P;
-        protected CryptoBuffer K;
+        protected final SRP state;
         protected boolean ok;
 
-        public Endpoint() {
-            DH.Params params = new DH.Params();
-            N = params.p;
-            g = params.g;
-            k = new BigInteger("3");
+        public Endpoint(String user, String password) {
+            state = new SRP();
+            state.setCredentials(user, password);
         }
 
         public boolean isAuthenticated() {
@@ -58,34 +49,23 @@ public class Set5Challenge36 {
     }
 
     public static class Client extends Endpoint {
-        private final BigInteger a;
-        private BigInteger A, B, salt;
+        private BigInteger serverPublicKey;
 
-        public Client(String password) {
-            super();
-            a = new BigInteger(512, r);
-            I = new CryptoBuffer("falken");
-            P = new CryptoBuffer(password);
+        public Client(String user, String password) {
+            super(user, password);
         }
 
         public void sendPublicKey(Server other) {
-            A = g.modPow(a, N);
-            other.receivePublicKey(I, A);
+            other.receivePublicKey(state.getI(), state.clientComputePublicKey());
         }
 
-        public void receivePublicKeyReply(BigInteger salt, BigInteger B) {
-            this.salt = salt;
-            this.B = B;
-            CryptoBuffer uH = new CryptoBuffer(A.toByteArray()).append(new CryptoBuffer(B.toByteArray())).sha256();
-            BigInteger u = new BigInteger(uH.toHex(), 16);
-            CryptoBuffer xH = new CryptoBuffer(salt.toByteArray()).append(P).sha256();
-            BigInteger x = new BigInteger(xH.toHex(), 16);
-            BigInteger S = B.subtract(k.multiply(g.modPow(x, N))).modPow((a.add(u.multiply(x))), N);
-            K = new CryptoBuffer(S.toByteArray()).sha256();
+        public void receivePublicKeyReply(BigInteger salt, BigInteger publicKey) {
+            state.setSalt(salt);
+            serverPublicKey = publicKey;
         }
 
         public void sendSessionKey(Server other) {
-            other.receiveSessionKey(MACs.hmacSha256(K, new CryptoBuffer(salt.toByteArray())));
+            other.receiveSessionKey(state.clientComputeSessionKey(serverPublicKey));
         }
 
         public void receiveSessionKeyReply(boolean ok) {
@@ -94,36 +74,23 @@ public class Set5Challenge36 {
     }
 
     public static class Server extends Endpoint {
-        private final BigInteger salt, v, b;
-        private BigInteger A, B;
+        private BigInteger clientPublicKey;
 
-        public Server() {
-            super();
-            b = new BigInteger(512, r);
-            I = new CryptoBuffer("falken");
-            P = new CryptoBuffer("joshua");
-
-            salt = new BigInteger(512, r);
-            CryptoBuffer xH = new CryptoBuffer(salt.toByteArray()).append(P).sha256();
-            BigInteger x = new BigInteger(xH.toHex(), 16);
-            v = g.modPow(x, N);
+        public Server(String user, String password) {
+            super(user, password);
+            state.serverComputeVerifier();
         }
 
-        public void receivePublicKey(CryptoBuffer I, BigInteger A) {
-            this.A = A;
+        public void receivePublicKey(CryptoBuffer I, BigInteger publicKey) {
+            clientPublicKey = publicKey;
         }
 
         public void sendPublicKeyReply(Client other) {
-            B = (k.multiply(v)).add(g.modPow(b, N));
-            other.receivePublicKeyReply(salt, B);
-            CryptoBuffer uH = new CryptoBuffer(A.toByteArray()).append(new CryptoBuffer(B.toByteArray())).sha256();
-            BigInteger u = new BigInteger(uH.toHex(), 16);
-            BigInteger S = A.multiply(v.modPow(u, N)).modPow(b, N);
-            K = new CryptoBuffer(S.toByteArray()).sha256();
+            other.receivePublicKeyReply(state.getSalt(), state.serverComputePublicKey());
         }
 
         public void receiveSessionKey(CryptoBuffer clientKey) {
-            CryptoBuffer serverKey = MACs.hmacSha256(K, new CryptoBuffer(salt.toByteArray()));
+            CryptoBuffer serverKey = state.serverComputeSessionKey(clientPublicKey);
             ok = serverKey.equals(clientKey);
         }
 
